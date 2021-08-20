63e60de4f105a8f79562ac15682cc457dfcc25a0
Haemin <rlagpals@gmail.com>
2021-08-20 19:28:47 +0900

    Defined FrameData to parse csv

---
+++ Assets/Scripts/Estimation/FrameData.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FrameData : MonoBehaviour
+{
+    public int frameNum;
+    public List<Vector3> jointPositions;
+
+
+}
