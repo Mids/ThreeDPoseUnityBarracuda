fe49839dfe00367f4c6436c02b39f010cf0b8512
Haemin <rlagpals@gmail.com>
2021-08-22 11:12:33 +0900

    Call and tested CSVReader

--- Assets/Scripts/Estimation/DataImporter.cs
+++ Assets/Scripts/Estimation/DataImporter.cs
@@ -4,10 +4,16 @@ using UnityEngine;
 
 public class DataImporter : MonoBehaviour
 {
+    public TextAsset data;
     // Start is called before the first frame update
     void Start()
     {
-        
+        CSVReader reader = new CSVReader();
+        List<FrameData> result = reader.ReadFrameData(data);
+
+        print(result[0]);
+        print(result[0].frameNum);
+        print(result[0].jointPositions[0].x);
     }
 
     // Update is called once per frame
