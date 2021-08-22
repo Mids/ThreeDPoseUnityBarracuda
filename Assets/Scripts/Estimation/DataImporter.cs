9f2f65b2a8ebc6681d474f512ae478a829d421fb
Haemin <rlagpals@gmail.com>
2021-08-22 12:32:44 +0900

    Draw joint position from videopose3D with spheres

--- Assets/Scripts/Estimation/DataImporter.cs
+++ Assets/Scripts/Estimation/DataImporter.cs
@@ -5,20 +5,45 @@ using UnityEngine;
 public class DataImporter : MonoBehaviour
 {
     public TextAsset data;
-    // Start is called before the first frame update
+    public List<FrameData> result;
+    private GameObject[] joints;
+    private int frame;
+    private int nof;
+    private int noj;
+
     void Start()
     {
         CSVReader reader = new CSVReader();
-        List<FrameData> result = reader.ReadFrameData(data);
+        result = reader.ReadFrameData(data);
+        
+        // get motion info
+        frame = 0;
+        nof = result.Count;
+        noj = result[0].jointPositions.Count;
+        
+        // initiate joints
+        joints = new GameObject[noj];
+        for(int j = 0; j < noj; j++)
+        {
+            var joint = GameObject.CreatePrimitive(PrimitiveType.Sphere);
+            joint.transform.localScale = new Vector3(0.3f, 0.3f, 0.3f);
+            var jointRenderer = joint.GetComponent<Renderer>();
+            jointRenderer.material.SetColor("_Color", Color.red);
+            joints[j] = joint;
+        }
 
-        print(result[0]);
-        print(result[0].frameNum);
-        print(result[0].jointPositions[0].x);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        // Draw estimation joint positions
+        for(int j = 0; j < noj; j++)
+        {
+            Vector3 pos = 5.0f * result[frame].jointPositions[j];
+            joints[j].transform.position = pos;
+        }
+
+        frame = (frame + 1) % nof;
     }
 }
